b01e3f100f742b84404f65299cb623fd81f8084b
jacobfriisstrand <jacob@friis.com>
2026-04-05 12:44:10 +0200

    fix: move use client directive before dynamic export

--- app/check-in/page.tsx
+++ app/check-in/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = "force-dynamic";
+"use client";
 
-("use client");
+export const dynamic = "force-dynamic";
 
 import { useCallback, useEffect, useState } from "react";
 import { Separator } from "@/components/ui/separator";
